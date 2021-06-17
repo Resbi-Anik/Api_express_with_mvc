c805f36481374fad2bae23acda316c84ba77b24b
Resbi-Anik <resbianik13@gmail.com>
2021-06-17 21:28:28 +0600

    api post, put and delete mothod

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
-const bike = [
+let bike = [
   {
     id: 1,
     name: "Honda",
@@ -27,6 +32,27 @@ app.get("/bikes/:id", (req, res) => {
   const getBike = bike.find((val) => val.id === bikeById);
   res.send(getBike);
 });
+
+app.post("/bikes/new-bike", (req, res) => {
+  let newBike = bike
+  newBike.push(req.body)
+  bike=newBike
+  res.send(bike);
+});
+
+app.delete("/bikes/:id",(req,res)=>{
+  let removeBikeId=parseInt(req.params.id);
+  let restBike = bike.filter(val => val.id!==removeBikeId)
+  console.log(restBike);
+  res.send(restBike)
+})
+
+app.put("/bikes/:id",(req,res)=>{
+  let updateBikeId=parseInt(req.params.id);
+  let updatedBike = bike.filter(val => val.id===updateBikeId )
+  updatedBike[0].name="walton"
+  console.log(bike);
+})
 
 app.listen(PORT, () => {
   console.log(`server is listening at PORT ${PORT}`);
