00002becf764a098b12730eeb41b5434ea832beb
Resbi-Anik <resbianik13@gmail.com>
2021-06-17 20:26:12 +0600

    get[simple, by id] request

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+const bike = [
+  {
+    id: 1,
+    name: "Honda",
+  },
+  {
+    id: 2,
+    name: "Yamaha",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("get request");
+});
+
+app.get("/bikes", (req, res) => {
+  res.send(bike);
+});
+
+app.get("/bikes/:id", (req, res) => {
+  const bikeById = parseInt(req.params.id);
+  const getBike = bike.find((val) => val.id === bikeById);
+  res.send(getBike);
+});
+
 app.listen(PORT, () => {
   console.log(`server is listening at PORT ${PORT}`);
 });
